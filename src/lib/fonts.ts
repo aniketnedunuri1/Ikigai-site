4d935d39cc5a236bf8af27af63c2dd937ff8ba4a
Areeb Khan <areebk@umich.edu>
2024-08-17 01:45:48 -0700

    Site push

---
+++ src/lib/fonts.ts
@@ -0,0 +1,15 @@
+import { Newsreader } from "next/font/google";
+
+export const newsreaderNormal = Newsreader({
+  weight: ['300'],
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const newsreaderItalic = Newsreader({
+  weight: ['300'],
+  style: 'italic',
+  subsets: ['latin'],
+  display: 'swap',
+});
