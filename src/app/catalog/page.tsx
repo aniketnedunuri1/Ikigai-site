22103846fe83147f48c317f4810b2db5ade11873
aniketn <aniketn16@gmail.com>
2024-08-29 21:33:55 -0400

    added sweats and tee

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -3,7 +3,7 @@ import Item from "./item";
 
 export default function Page() {
   return (
-    <section className="flex flex-col justify-center items-center min-h-screen space-y-8">
+    <section className="flex flex-col justify-center items-center min-h-screen space-y-8 mt-20">
       {/* Italic text at the top center */}
       <div className="text-center">
         <p className="italic text-lg">
@@ -13,9 +13,11 @@ export default function Page() {
       </div>
 
       {/* Items */}
-      <div className="flex space-x-8">
+      <div className="grid grid-cols-2 grid-x-2">
         <Item src1="/catalog/hoodie_front.png" src2="/catalog/hoodie_back.png" title="HOODIE" />
         <Item src1="/catalog/crew_front.png" src2="/catalog/crew_back.png" title="CREW"/>
+        <Item src1="/catalog/ikigai_pants_front.png" src2="/catalog/ikigai_pants_back.png" title="SWEATS"/>
+        <Item src1="/catalog/shirt_front.png" src2="/catalog/shirt_back.png" title="TEE"/>
       </div>
     </section>
   );
