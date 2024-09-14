a545b6ee5a6820c78683ed1b8f29a714aab8595b
Areeb Khan <areebk@umich.edu>
2024-09-13 23:52:53 -0400

    changed analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,20 +21,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PF15DYQ4G5"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-T9XSS3M9');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PF15DYQ4G5');
             `,
           }}
-        />
-        {/* End Google Tag Manager */}
-      </head>
+        />      </head>
       <body className={inter.className + " bg-white"}>
           <noscript
           dangerouslySetInnerHTML={{
